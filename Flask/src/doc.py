20809ada7edb9288165a4f2a9075aa9bf1183355
XzavierDunn <xmandunn@gmail.com>
2019-07-23 19:52:11 -0400

    f

--- Flask/src/doc.py
+++ Flask/src/doc.py
@@ -1,7 +1,7 @@
 import tweepy
 import os
 
-auth = tweepy.OAuthHandler(os.environ['consumer_key'], os.environ['consumer_secret'])
-auth.set_access_token(os.environ['access_token'], os.environ['access_token_secret'])
+auth = tweepy.OAuthHandler('zPKVwvIEQh8g0nJnQEH0ZEQjB', 'G2mXQMFFJRvXHbhrDLNRFwLWjDm8P3VIwVxwmWspxyPfQ8q   H1e')
+auth.set_access_token('2409002919-M5pmvGY9hfakVDvL5ZgvMJuXBm92TNdvLULnLmF   ', 'WkaMbwO0g7sxZYWjo76qcmpxcueRd2J69jY4iyNfLS2   XD')
 
 tweepyAPI = tweepy.API(auth)
