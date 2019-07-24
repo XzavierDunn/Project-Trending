04f9399141d43a2535e0c6f870cf9b3b57e8a00d
XzavierDunn <xmandunn@gmail.com>
2019-07-24 11:19:44 -0400

    f

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from proj.src.app import create_app
 
 env_name = os.getenv('FLASK_ENV')
-app = create_app(env_name)
+application = create_app(env_name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
